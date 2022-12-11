448076306fbfa81e789dc9c345bf2b2832ebb156
Oladapo <Oladapo>
2022-12-11 14:58:08 -0800

    Python is nice

--- 0x04-python-more_data_structures/7-main.py
+++ 0x04-python-more_data_structures/7-main.py
@@ -1,4 +1,4 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 update_dictionary = __import__('7-update_dictionary').update_dictionary
 print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
