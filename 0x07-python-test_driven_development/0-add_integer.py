b4c202dfe1d87622e9320313912cc8d330baabeb
Oladapo <okikiolaoladapo10@gmail.com>
2022-12-30 00:57:56 +0100

    move on

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#  Author: Oladapo Okikiola
+def add_integer(a, b=98):
+
+    """
+    Addition function
+    
+    Args:
+    a: integer or float
+    b: integer or float
+    
+    Return a + b
+    """
+    return (a + b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,4 @@
+# 0x07-python-test_driven_development
+We are gonna learn how to test our python codes
+### Hurray!
+## Author: Oladapo Okikiola
